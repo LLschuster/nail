d34ee282299231227356db6978a060320563c022
llschuster <perez99leo@outlook.es>
2020-05-03 15:49:03 +0200

    Statistics shows list of finished workouts

--- lib/repositories/workoutRepository.dart
+++ lib/repositories/workoutRepository.dart
@@ -1,31 +1,28 @@
-
-
 import 'package:nail/db/dbconfig.dart';
 import 'package:nail/models/workout.dart';
 
 class WorkoutRepository {
-  Map<String, dynamic> getWorkout(){
-
+  Map<String, dynamic> getWorkout() {
     var workoutDetail = [
       [
-          {"name": "Push ups", "reps": "25x", "img": "push-ups"},
-          {"name": "Pull ups", "reps": "20x", "img": "pull-ups"},
-          {"name": "Sit ups", "reps": "18x", "img": "sit-ups"},
-        ],
+        {"name": "Push ups", "reps": "25x", "img": "push-ups"},
+        {"name": "Pull ups", "reps": "20x", "img": "pull-ups"},
+        {"name": "Sit ups", "reps": "18x", "img": "sit-ups"},
+      ],
       [
-          {"name": "Push ups", "reps": "25x", "img": "push-ups"},
-          {"name": "Pull ups", "reps": "2 min", "img": "pull-ups"},
-          {"name": "Sit ups", "reps": "18x", "img": "sit-ups"},
-        ],
+        {"name": "Push ups", "reps": "25x", "img": "push-ups"},
+        {"name": "Pull ups", "reps": "2 min", "img": "pull-ups"},
+        {"name": "Sit ups", "reps": "18x", "img": "sit-ups"},
+      ],
       [
         {"name": "Push ups", "reps": "25x", "img": "push-ups"},
         {"name": "extreme push ups", "reps": "20x", "img": "push-ups"},
         {"name": "Sit ups", "reps": "1 min", "img": "sit-ups"},
-        ],
-      ];
+      ],
+    ];
 
     var workout = {
-      "name" : "full body workout",
+      "name": "full body workout",
       "id": 001,
       "workoutDetail": workoutDetail,
     };
@@ -33,14 +30,28 @@ class WorkoutRepository {
     return workout;
   }
 
-  Future<Map<String, dynamic>> saveFinishedWorkout(Map<String, dynamic> doneWorkout, double difficultyLevel)async {
+  Future<Map<String, dynamic>> getFinishedWorkout() async {
     try {
-    FinishWorkout workout = FinishWorkout.toSave(workoutId: doneWorkout["id"],
-     difficultyLevel: difficultyLevel, name: doneWorkout["name"], workoutDetail: doneWorkout["workoutDetail"].toString());
-    await insertFinishedWorkout(workout);
-    var test = await getFinishedWorkouts();
-    return {"success": true};
-    } catch(e){
+      var finishedWorkouts = await getFinishedWorkouts();
+      return {"success": true, "workoutList": finishedWorkouts};
+    } catch (e) {
+      print(e);
+      return {"error": "workout could not be saved;"};
+    }
+  }
+
+  Future<Map<String, dynamic>> saveFinishedWorkout(
+      Map<String, dynamic> doneWorkout, double difficultyLevel) async {
+    try {
+      FinishWorkout workout = FinishWorkout.toSave(
+          workoutId: doneWorkout["id"],
+          difficultyLevel: difficultyLevel,
+          name: doneWorkout["name"],
+          workoutDetail: doneWorkout["workoutDetail"].toString());
+      await insertFinishedWorkout(workout);
+      var test = await getFinishedWorkouts();
+      return {"success": true};
+    } catch (e) {
       print(e);
       return {"error": "workout could not be saved;"};
     }
--- lib/ui/statistics.dart
+++ lib/ui/statistics.dart
@@ -2,12 +2,89 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:nail/models/workout.dart';
+import 'package:nail/repositories/workoutRepository.dart';
 
-class Statistics extends StatelessWidget {
+class Statistics extends StatefulWidget {
   @override
+  State<Statistics> createState() {
+    // TODO: implement createState
+    return StatisticsState();
+  }
+}
+
+class StatisticsState extends State<Statistics> {
+  WorkoutRepository workoutRepository;
+  Map<String, dynamic> finishedWorkouts;
+  bool _loading = false;
+
+
+  @override
+  void initState() {
+    super.initState();
+    workoutRepository = WorkoutRepository();
+    setupData();
+    _loading = true;
+  }
+
+  Future<void> setupData() async{
+    var workouts = await workoutRepository.getFinishedWorkout();
+    setState(() {
+      finishedWorkouts = workouts;
+      _loading = false;
+    });
+  }
+    @override
   Widget build(BuildContext context) {
+
+    Widget finishedWorkoutsList() {
+      if (!_loading){
+print(finishedWorkouts);
+      List<FinishWorkout> workouts = finishedWorkouts["workoutList"];
+      List<Widget> workoutsRows = [
+        Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Text("Workout", style: TextStyle(fontSize: 14),),
+              Text("Difficulty", style: TextStyle(fontSize: 14),),
+              Text("Date", style: TextStyle(fontSize: 14),),
+            ]
+          )
+      ];
+      for (int i=0; i<workouts.length; ++i){
+        workoutsRows.add(
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 10.0),
+            child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Text("${workouts[i].name}", style: TextStyle(fontSize: 14),),
+              Text("${workouts[i].difficultyLevel}", style: TextStyle(fontSize: 14),),
+              Text("${DateTime.fromMillisecondsSinceEpoch(workouts[i].time * 1000)}", style: TextStyle(fontSize: 14),),
+            ]
+          )
+          )
+          
+        );
+      }
+
+      return Container(
+      padding: EdgeInsets.all(5.0),
+      child: Column(
+        children: workoutsRows
+      ),
+    );
+      } else {
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      }
+      
+    }
+
+
     return Container(
-      child: const Text('statistics')
+      child: finishedWorkoutsList()
     );
   }
 }
